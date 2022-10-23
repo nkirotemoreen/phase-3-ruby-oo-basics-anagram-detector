9963ff2dc23f07b24fe3c2205b9a4c3e222d6a4a
nkirotemoreen <nkirotemoreen70@gmail.com>
2022-10-23 05:55:45 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+#Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    
+    end
+    def match(array)
+        array.select{|similar| similar.split("").sort == @word.split('').sort}
+    end
+end
+anagrawrd = Anagram.new('listen')
+puts anagrawrd.word
+
